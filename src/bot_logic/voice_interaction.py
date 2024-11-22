e9a6fb44beac68c4dc64327922f903feaaa3e69b
AIbdullah <mabdullahsaqib2004@gmail.com>
2024-11-22 21:16:40 +0500

    debugged trade routing

--- src/bot_logic/voice_interaction.py
+++ src/bot_logic/voice_interaction.py
@@ -12,13 +12,13 @@ user_commands = ['data', 'username', 'change']
 
 def handle_user_command(command, data= None):
     """Handle the user's command and return the appropriate response."""
-    if 'wallet' in command.lower() and any(cmd in command.lower() for cmd in wallet_commands):
+    if 'wallet' in command.lower() or any(cmd in command.lower() for cmd in wallet_commands):
         return wallet_voice_interaction(command, data)
-    elif 'trade' in command.lower() and any(cmd in command.lower() for cmd in trade_commands):
+    elif 'trade' in command.lower() or any(cmd in command.lower() for cmd in trade_commands):
         return trade_voice_interaction(command,data)
-    elif 'configuration' in command.lower() and any(cmd in command.lower() for cmd in config_commands):
+    elif 'configuration' in command.lower() or any(cmd in command.lower() for cmd in config_commands):
         return config_voice_interaction(command,data)
-    elif 'user' in command.lower() and any(cmd in command.lower() for cmd in user_commands):
+    elif 'user' in command.lower() or any(cmd in command.lower() for cmd in user_commands):
         return user_voice_interaction(command, data)
     elif 'logout' in command.lower() or 'exit' in command.lower():
         logout()
